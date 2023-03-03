ad09f47aed66e57af0834766d09f92d0f115fdea
raffaez <rafaeleasouzas@gmail.com>
2023-03-03 00:18:33 -0300

    feat: add inverter string

---
+++ inverter-string/index.js
@@ -0,0 +1,15 @@
+// 5) Escreva um programa que inverta os caracteres de um string.
+
+// IMPORTANTE:
+// a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+// b) Evite usar funções prontas, como, por exemplo, reverse;
+
+const inverterString = (str) => {
+  let strInvertida = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    strInvertida += str[i];
+  }
+  return strInvertida;
+};
+
+console.log(inverterString("hello world"));
